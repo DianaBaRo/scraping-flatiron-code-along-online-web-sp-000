073fbdf1649859a8bb57fbb0979a05d8752498c0
Diana Badas <diana.badas@gmail.com>
2019-01-30 15:28:08 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -19,21 +19,20 @@ class Scraper
 =end
   end
 
- def get_courses
+  def get_courses
 
- end
-
-
+  end
+ 
   def print_courses
     self.make_courses
-    Course.all.each do |course|
-      if course.title
-        puts "Title: #{course.title}"
-        puts "  Schedule: #{course.schedule}"
-        puts "  Description: #{course.description}"
+      Course.all.each do |course|
+        if course.title
+          puts "Title: #{course.title}"
+          puts "  Schedule: #{course.schedule}"
+          puts "  Description: #{course.description}"
+        end
       end
-    end
-  end
+   end
 
 end
 
